392d8b81f4d55e3d34fbaa98539a9a3960c92266
Aris Ripandi <ar.is@outlook.com>
2020-03-12 12:43:27 +0700

    change logout redirect path

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -19,7 +20,9 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        logout as doLogout;
+    }
 
     /**
      * Where to redirect users after login.
@@ -36,5 +39,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Set the logout redirect path.
+     *
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        $this->doLogout($request);
+
+        return redirect()->route('login');
     }
 }
